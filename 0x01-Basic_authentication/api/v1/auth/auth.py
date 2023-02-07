d6ccc25183be6d314d7999567cb654a40f378a15
root <professionaleditor6@gmail.com>
2023-02-07 17:12:49 +0000

    update code auth.py

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -12,11 +12,12 @@ class Auth:
         if path is None or excluded_paths is None or excluded_paths == []:
             return True
         for paths in excluded_paths:
-            if path.startswith(paths):
+            if path.startswith(paths) or paths.startswith(path):
                 return False
             if paths[-1] == '*':
                 if path.startswith(paths[:-1]):
                     return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         ''' Auth public method that returns authorization header'''
