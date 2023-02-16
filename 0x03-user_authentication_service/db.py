38db7109ddc3cbf046dc6e5d347e1ba08aee50af
root <professionaleditor6@gmail.com>
2023-02-16 14:33:30 +0000

    update db.py

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -49,27 +49,25 @@ class DB:
         filterd by method's imput arguments'''
 
         if not kwargs:
-            raise InvalidRequestError
+            raise InvalidRequestError()
         req = User.__table__.columns.keys()
         for key in kwargs.keys():
             if key not in req:
-                raise InvalidRequestError
+                raise InvalidRequestError()
 
         user = self._session.query(User).filter_by(**kwargs).first()
         if user is None:
-            raise NoResultFound
+            raise NoResultFound()
         return user
 
     def update_user(self, user_id: int, **kwargs: dict) -> None:
         '''locates user and update the user's attributes
         as passed in the method's args'''
-        if not isinstance(user_id, int):
-            return
         user = self.find_user_by(id=user_id)
         req = User.__table__.columns.keys()
         for key in kwargs.keys():
             if key not in req:
-                raise ValueError
+                raise ValueError()
         for key, value in kwargs.items():
             setattr(user, key, value)
         self._session.commit()
